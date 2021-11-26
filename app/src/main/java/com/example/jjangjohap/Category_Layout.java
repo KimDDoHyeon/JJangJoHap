259e18b0896b832549026ea9aca251831e47ce44
KimDoHyeon <qmffor1317@gmail.com>
2021-11-26 13:55:11 +0900

    카테고리 레이아웃 기본 구성
    - RecyclerView에 들어갈 것들은 주말에 작업 예정

---
+++ app/src/main/java/com/example/jjangjohap/Category_Layout.java
@@ -0,0 +1,14 @@
+package com.example.jjangjohap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Category_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_layout);
+    }
+}
